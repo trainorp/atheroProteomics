441461ad9f9042d3a9a9bc4509e4dee62a2eedc3
trainorp <patrick.trainor@louisville.edu>
2018-05-15 15:08:05 -0400

    idk

--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
 ########### Prereqs ###########
 options(stringsAsFactors = FALSE,scipen = 600)
 library(tidyverse)
+library(gridExtra)
 
 setwd("~/gdrive/AthroProteomics")
 peptides<-read.csv('peptide_table_20180514.csv')
@@ -24,23 +25,28 @@ groups$Group<-"sCAD"
 groups$Group[!is.na(groups$MIGroup)]<-groups$MIGroup[!is.na(groups$MIGroup)]
 
 # Wide data:
-peptidesW<-peptides %>% select(-Quality.Score,-(Parent.Protein:Percent.Files.With.Good.Quant)) %>%
-  gather(key="rep",value="value",-Name)
-peptidesW<-pheno %>% left_join(groups) %>% left_join(peptidesW,by=c("newName"="rep"))
-peptidesW$GroupTime<-paste(peptidesW$Group,peptidesW$timept,sep=".")
-peptidesW$GroupTime<-factor(peptidesW$GroupTime,
-  levels=c("sCAD.FU","sCAD.T0","Type 1.FU","Type 1.T0","Type 2.FU","Type 2.T0",
-           "Indeterminate.FU","Indeterminate.T0"))
-
-peptidesW<-peptidesW %>% arrange(GroupTime,ptid,replicate)
-temp1<-peptidesW %>% select(GroupTime,ptid,replicate) %>% unique()
-temp1$uID<-as.factor(1L:nrow(temp1))
-peptidesW<-temp1 %>% left_join(peptidesW)
+makeWideFun<-function(data){
+  peptidesW<-data %>% select(-Quality.Score,-(Parent.Protein:Percent.Files.With.Good.Quant)) %>%
+    gather(key="rep",value="Intensity",-Name)
+  peptidesW<-pheno %>% left_join(groups) %>% left_join(peptidesW,by=c("newName"="rep"))
+  peptidesW$GroupTime<-paste(peptidesW$Group,peptidesW$timept,sep=".")
+  peptidesW$GroupTime<-factor(peptidesW$GroupTime,
+                              levels=c("sCAD.FU","sCAD.T0","Type 1.FU","Type 1.T0","Type 2.FU","Type 2.T0",
+                                       "Indeterminate.FU","Indeterminate.T0"))
+  
+  peptidesW<-peptidesW %>% arrange(GroupTime,ptid,replicate)
+  temp1<-peptidesW %>% select(GroupTime,ptid,replicate) %>% unique()
+  temp1$uID<-as.factor(1L:nrow(temp1))
+  peptidesW<-temp1 %>% left_join(peptidesW)
+  return(peptidesW)
+}
+peptidesW<-makeWideFun(peptides)
 
 ########### Plots ###########
-png(file="plots/peptideNoNorm.png",height=5,width=15,units="in",res=300)
-ggplot(data=peptidesW,aes(x=uID,group=newName,color=GroupTime,y=log2(value)))+
-         geom_boxplot()+theme_bw()
+png(file="plots/peptideNoNorm.png",height=5,width=10,units="in",res=300)
+p0<-ggplot(data=peptidesW,aes(x=uID,group=newName,color=GroupTime,y=log2(Intensity)))+
+         geom_boxplot()+theme_bw()+xlab("")+theme(axis.text.x=element_blank())
+show(p0)
 dev.off()
 
 ########### Normalization ###########
@@ -49,3 +55,18 @@ m0<-as.matrix(peptides[,grepl("rep",names(peptides))])
 # Columnwise total intensity normalization:
 cSums<-apply(m0,2,sum)
 cFac<-cSums/mean(cSums)
+m1<-m0
+for(i in 1:ncol(m1)) m1[,i]<-m1[,i]/cFac[i]
+peptides1<-peptides
+peptides1[,grepl("rep",names(peptides1))]<-m1
+peptidesW1<-makeWideFun(peptides1)
+
+png(file="plots/peptideColNorm.png",height=5,width=10,units="in",res=300)
+p1<-ggplot(data=peptidesW1,aes(x=uID,group=newName,color=GroupTime,y=log2(Intensity)))+
+  geom_boxplot()+theme_bw()+xlab("")+theme(axis.text.x=element_blank())
+show(p1)
+dev.off()
+
+png(file="plots/peptideNoneVColNorm.png",height=10,width=10,units="in",res=300)
+grid.arrange(p0,p1,nrow=2)
+dev.off()
