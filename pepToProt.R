bc464b4e0f1389725363ad27875cc7da7efbc80d
trainorp <patrick.trainor@louisville.edu>
2018-05-25 11:37:13 -0400

    idk

--- pepToProt.R
+++ pepToProt.R
@@ -5,16 +5,34 @@ library(tidyverse)
 setwd("~/gdrive/AthroProteomics/data/")
 
 # Import data:
-pepToProt<-read.csv("pepToProt_20180524.csv",header=FALSE,skip=3)
+pepToProt<-read.csv("pepToProt_20180524.csv",header=TRUE)
 srma<-read.csv("HumanSRMAtlasPeptidesFinalAnnotated.csv",header=TRUE)
 # Names & human only:
 names(pepToProt)<-c("pepSeq","proteins")
-pepToProt<-pepToProt[grepl("HUMAN",pepToProt$proteins),]
+# Find e coli beta gal / otherwise filter for Human only:
+pepToProt<-pepToProt[grepl("HUMAN",pepToProt$proteins)|
+                       grepl("P00722",pepToProt$protein),]
+pepToProt<-pepToProt[!duplicated(pepToProt),]
 # Other peptide annotation data:
 load("pepAnno.RData")
+nrow(pepAnno) #3,544
+
+########### Collapse proteins ###########
+pepAnno$pepSeq<-gsub("(\\[.*?\\])","",pepAnno$Name)
+pepAnno<-pepAnno %>% left_join(pepToProt)
+
+uniquePep<-unique(pepAnno$Name)
+# Collapse function
+protCollapse<-function(pep){
+  temp<-pepAnno[pepAnno$Name==pep,]
+  temp2<-paste(temp$proteins,collapse=";")
+  return(cbind(temp[1,names(temp)!="proteins"],
+               data.frame(proteins=temp2,protN=length(temp$proteins))))
+}
+protCollapseDF<-lapply(uniquePep,protCollapse)
+pepAnno<-do.call("rbind",protCollapseDF)
+nrow(pepAnno) #3,544
 
 ########### Counts ###########
-pepAnno<-pepAnno %>% left_join(pepToProt,by=c("Name"="pepSeq"))
-idk<-pepToProt %>% group_by(proteins) %>% mutate(n())
-
-pepToProt<-pepToProt %>% left_join(srma,by=c("pepSeq"="sequence"))
+pepAnno2<-pepAnno %>% left_join(srma,by=c("pepSeq"="sequence"))
+save(pepAnno2,file="pepAnno2.RData")
