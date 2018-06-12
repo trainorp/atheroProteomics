ecf0c087ed0419ec73e96d7ca393666755f3128b
trainorp <patrick.trainor@louisville.edu>
2018-06-12 14:41:27 -0400

    idk

--- peptideMatch.sh
+++ peptideMatch.sh
@@ -1,7 +1,5 @@
-java -jar PeptideMatchCMD_1.0.jar -a index -d uniprot_sprot_wvar.fasta -i sprot_index_wvar
-
 var="$(cat pepSeqsStr.txt)"
 
-java -jar PeptideMatchCMD_1.0.jar -a query -i sprot_index_wvar -q $var -o out.txt 
+java -jar PeptideMatchCMD_1.0.jar -a query -i sprot_index -q $var -o out.txt 
 
 
